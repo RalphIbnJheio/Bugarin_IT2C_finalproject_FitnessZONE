9f6c77f641c0d6da176084d0a00a79248796ce03
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-11 10:41:41 +0800

    initial files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="">HOME</a></li>	
+				<li><a href="">FITNESS</a></li>
+				<li><a href="">NUTRITION</a></li>	
+				<li><a href="">PROGRAM</a></li>	
+				<li><a href="quotes.php">QUOTES</a></li>	
+			</ul>
+		</nav>
+		<div class="main">
+			<h1>FITNESS <span> ZONE </span></h1>
+			<div class="paragraph">
+				<?php
+					echo "It’s often easier to figure out how not to do or be something than it is to figure out how to do or be something.", "<br/>";
+					echo "Find everything you need to crush your fitness goals such as workout routines, training plans, free workout videos,", "<br/>";
+					echo "fitness tips, exercise trends, workout playlists, gear, clothes and more.";
+				?>
+			</div>
+		</div>
+		<div class="images">
+			<img src="photos/abstract.PNG" class="abstract">
+			<img src="photos/girl.PNG" class="girl">
+		</div>
+	</div>
+</body>
---
+++ quotes.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="logo.PNG" class="logo">
+			<ul>
+				<li><a href="index.php">HOME</a></li>	
+				<li><a href="">FITNESS</a></li>
+				<li><a href="">NUTRITION</a></li>	
+				<li><a href="">PROGRAM</a></li>	
+				<li><a href="quotes.php">QUOTES</a></li>	
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1>MOTIVATIONAL <span> QUOTES </span></h1>
+		</div>
+		<div class="main-images">
+			<center><img src="photos/ROCK.JPG"></center>
+			<div class="image-description">
+				<h2><b>Dwayne “The Rock” Johnson</b></h2>
+				<?php
+					echo "“Success isn’t always about ‘greatness.’ It’s about consistency. Consistent hard work gains success. Greatness will come.”";
+				?>
+			</div>
+
+			<img src="photos/LEE.JPG">
+			<div class="image-description">
+				<h2><b>Bruce Lee</b></h2>
+				<?php
+					echo "“I fear not the man who has practiced 10000 kicks once, but I fear the man who has practiced one kick 10000 times.” ";
+				?>
+			</div>
+
+			<img src="photos/waldo.JPG">
+			<div class="image-description">
+				<h2><b>Ralph Waldo Emerson</b></h2>
+				<?php
+					echo "“Few people know how to take a walk. The qualifications are endurance, plain clothes, old shoes, an eye for nature, good humor, vast curiosity, good speech, good silence and nothing too much.”";
+				?>
+			</div>
+
+			<img src="photos/phelps.JPG">
+			<div class="image-description">
+				<h2><b>Michael Phelps</b></h2>
+				<?php
+					echo "“When I feel tired, I just think about how great I will feel once I finally reach my goal.”";
+				?>
+			</div>
+
+			<img src="photos/Arnold.JPG">
+			<div class="image-description">
+				<h2><b>Arnold Schwarzenegger</b></h2>
+				<?php
+					echo "“The resistance that you fight physically in the gym and the resistance that you fight in life can only build a strong character.”";
+				?>
+			</div>
+		
+	</div>
+</body>
