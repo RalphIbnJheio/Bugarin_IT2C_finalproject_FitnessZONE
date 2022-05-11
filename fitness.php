7d50c9f279997262ca894ad12b6a9f69197bea78
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-11 12:20:53 +0800

    fitness file has been added

--- fitness.php
+++ fitness.php
@@ -10,7 +10,7 @@
 <body>
 	<div class="wrapper-2">
 		<nav>
-			<img src="logo.PNG" class="logo">
+			<img src="photos/logo.PNG" class="logo">
 			<ul>
 				<li><a href="index.php">HOME</a></li>	
 				<li><a href="fitnes.php">FITNESS</a></li>
@@ -20,48 +20,18 @@
 			</ul>
 		</nav>
 		<div class="main1">
-			<h1>MOTIVATIONAL <span> QUOTES </span></h1>
+			<h1>FITNESS <span> DIARY </span></h1>
 		</div>
-		<div class="main-images">
-			<center><img src="photos/ROCK.JPG"></center>
-			<div class="image-description">
-				<h2><b>Dwayne “The Rock” Johnson</b></h2>
-				<?php
-					echo "“Success isn’t always about ‘greatness.’ It’s about consistency. Consistent hard work gains success. Greatness will come.”";
-				?>
-			</div>
+		<div class="fitnesstable">
+			<h2>FITNESS</h2>
+			<ul>
+				<li><a href="quotes.php">Beginner Fitness Tips</a></li>
+				<li><a href="#Transportation">Workout Clothes</a></li>
+				<li><a href="#Events">HIIT</a></li>
+			</ul>			
+		</div>
 
-			<img src="photos/LEE.JPG">
-			<div class="image-description">
-				<h2><b>Bruce Lee</b></h2>
-				<?php
-					echo "“I fear not the man who has practiced 10000 kicks once, but I fear the man who has practiced one kick 10000 times.” ";
-				?>
-			</div>
-
-			<img src="photos/waldo.JPG">
-			<div class="image-description">
-				<h2><b>Ralph Waldo Emerson</b></h2>
-				<?php
-					echo "“Few people know how to take a walk. The qualifications are endurance, plain clothes, old shoes, an eye for nature, good humor, vast curiosity, good speech, good silence and nothing too much.”";
-				?>
-			</div>
-
-			<img src="photos/phelps.JPG">
-			<div class="image-description">
-				<h2><b>Michael Phelps</b></h2>
-				<?php
-					echo "“When I feel tired, I just think about how great I will feel once I finally reach my goal.”";
-				?>
-			</div>
-
-			<img src="photos/Arnold.JPG">
-			<div class="image-description">
-				<h2><b>Arnold Schwarzenegger</b></h2>
-				<?php
-					echo "“The resistance that you fight physically in the gym and the resistance that you fight in life can only build a strong character.”";
-				?>
-			</div>
+		</div>
 		
 	</div>
 </body>
