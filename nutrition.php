94512f6103ef9e498a387cbcbca09c8c0123cf9d
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-12 12:44:55 +0800

    nutrition file has been added

---
+++ nutrition.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="index.php">HOME</a></li>	
+				<li><a href="fitness.php">FITNESS</a></li>
+				<li><a href="nutrition.php">NUTRITION</a></li>	
+				<li><a href="program.php">PROGRAM</a></li>	
+				<li><a href="quotes.php">QUOTES</a></li>	
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1>HEALTHY <span> LIVING </span></h1>
+		</div>
+		<div class="imgLeft"></div>
+			<?php
+
+			 echo"<h3><b>Healthy Eating Tips</b></h3>	<p>Plenty of people would probably love to buy organic produce, which is grown without exposure to pesticides. But that's not always possible organic produce is often much more expensive than conventionally grown fruits and vegetables, and it's not always as accessible. That's why we're interested in the Environmental Working Group's annual Dirty Dozen list. It names the 12 types of produce that are most important to buy organic and has a Clean Fifteen list of the fruits and vegetables that are fine to buy conventional.</p>", "<br>";
+			 echo "<p>It's hard to imagine why anyone would disparage the simple banana. After all, they're tasty, versatile, and a good source of fiber, potassium, magnesium, and vitamins like B6 and C. But recently, dietitian-nutritionist Jennifer Cholewka, RD, CNSC, CDCES, CDN has been hearing people say that bananas are a surefire way to derail a weight loss plan. The reason? Apparently, the popular fruit is too sugary. While it's true that bananas have 15 grams of sugar, Cholewka takes issue with the idea that a banana's sugar content is a problem in some way. Whether you're trying to achieve weight loss or [improve your] overall health, increasing your fruit and vegetable intake is always a good strategy, she says.</p>", "<br>";
+			?>
+		<div class="imgRight"></div>
+			<?php
+			 echo"<h3><b>Low Carbs</b></h3> <p>Your food choices depend on a few things, including how healthy you are, how much you exercise and how much weight you have to lose.</p>", "<br>";
+			 echo"<p>Consider this meal plan as a general guideline, not something written in stone.</p>", "<br>";
+			 echo"<p>On a low carb diet, it can take a few days to overcome unpleasant symptoms and several weeks for your body to fully adapt. It’s important to be patient and not to abandon your diet too soon. A low-carb diet limits carbohydrates such as those found in grains, starchy vegetables and fruit and emphasizes foods high in protein and fat. Many types of low-carb diets exist. Each diet has varying restrictions on the types and amounts of carbohydrates you can eat. A low-carb diet is generally used for weight loss. Some low-carb diets may have health benefits beyond weight loss, such as reducing your risk of type 2 diabetes and metabolic syndrome.</p>", "<br>";
+			 echo "If you're eating low-carb, fast, flavorful dinner recipes are essential and that's exactly what you'll find on this list. These meals run the gamut from lettuce wraps to soups to one-dish wonders, but they all have two things in common: they're ready in 30 minutes or less, and they'll help keep you within your daily budget for carbs.", "<br>";
+
+			?>
+
+		<div class="imgLeft1"></div>
+			<?php
+			 echo "<h3><b>High Protein</b></h3> <p>Protein rich foods are famous for their weight loss properties. But did you know that including protein rich foods in your breakfasts can further be helpful in achieving quick weight loss? Proteins perform the function of reducing appetite and curbing cravings. Also, they help in building muscle mass and these are the top reasons why they are recommended and included in some of the most popular weight loss diets. Studies have found that including protein in your breakfast can help you eat lesser during rest of the day.</p>", "<br>";
+			 echo "<p>Proteins help in making you feel full for longer. It activates the body's signals that reduce appetite, overeating and cravings. This occurs because of a drop in hunger hormone ghrelin and a rise in peptide, GLP-1, cholecystokinin and YY - the fullness hormones</p>.", "<br>";
+			 echo "<p>What's more is that eating breakfasts with more protein can help you cut belly fat. Protein from foods sources have been found to be inversely related to belly fat. This means that the more high protein foods you eat, the lesser will your belly fat be. Studies have found that eating protein for breakfast can help in weight loss, especially in those who have a lot of weight to lose.</p>", "<br>";
+			 echo "<p>So, firstly make sure that you don't skip breakfast as it is the most important meal of the day. Secondly, include protein-rich foods in your breakfast like eggs, fish, milk, seafood, meat, nuts, seeds and tofu to name a few. These foods will be an amazing addition to your weight loss diet and will make losing weight much easier for you.</p>";
+
+
+			?>
+		<div class="nutritioncontent">
+			
+			
+			
+		</div>
+
+		</div>
+		
+	</div>
+</body>
