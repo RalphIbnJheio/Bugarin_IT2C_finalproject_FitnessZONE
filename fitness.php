f785050fbf7cba2296a928b89f6815cbe70aabd8
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-12 17:41:07 +0800

    modified

--- fitness.php
+++ fitness.php
@@ -29,6 +29,14 @@
 				<li><a href="#Transportation">Workout Clothes</a></li>
 				<li><a href="#Events">HIIT</a></li>
 				<center><b>GOOD TRIVIA FACTS!</b></center>
+				<?php 
+					echo "1. The human body has 650 muscles.", "<br>";
+					echo "2. The only exercise you should hold your breath for is underwater swimming.", "<br>";
+					echo "3. The heart is the strongest muscle in the body.", "<br>";
+					echo "4. Only 13% of men are physically fit.", "<br>";
+					echo "5. On average, a person walks 70,000 miles in their lifetime.", "<br>";
+
+				?>
 			</ul>	
 
 		</div>	
@@ -39,5 +47,7 @@
 			<li><a href="https://www.fitnessfirst.com.ph/membership/individual" target="_blank"><img src="photos/rightside1.JPG"></a></li>
 			<li><a href="https://theclubkona.com/" target="_blank"><img src="photos/rightside2.PNG"></a></li>
 		</ul>	
+	</div>
+	<div class="left-side-bar">
 	</div>
 </body>
