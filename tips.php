01a5bbf378ac0c3698317f7a57bdb19789ab704b
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-13 13:04:32 +0800

    File tips has been added

---
+++ tips.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="index.php">HOME</a></li>	
+				<li><a href="fitness.php">FITNESS</a></li>
+				<li><a href="nutrition.php">NUTRITION</a></li>	
+				<li><a href="program.php">PROGRAM</a></li>	
+				<li><a href="quotes.php">QUOTES</a></li>	
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1> 10 Beginners Fitness<span> Tips </span></h1>
+		</div>
+		<div class="Ftips">
+			<?php
+
+			 echo"<h3><b>1. SET REALISTIC GOALS</b></h3> <p>It may take some time to get the results you want. It’s vital to break your ultimate goal down in to stages and set lots of smaller goals to keep you motivated along the way.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>2. BE CONSISTENT</b></h3> <p>Results come through regular and consistent activity. Stick to your programme and avoid frequent stops and starts.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>3. TOO MUCH TOO SOON</b></h3> <p>Do not increase the amount of exercise you do too soon. Increase what you’re doing by no more than 10% per week.</p>"; "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>4. ACCEPT FEELINGS OF DISCOMFORT</b></h3> <p>When you first start exercising you’ll experience feelings of discomfort such as shortness of breath, sweating and aching muscles after exercising. Don’t worry, this is completely normal and everyone feels like this when they’re first starting out.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>5. TRAIN WITH A FRIEND</b></h3> <p>Training with a friend not only keeps you motivated during the sessions themselves but will also make you less likely to miss a planned session as you’re unlikely to want to let down your friend.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>6. WARM UP</b></h3> <p>Not warming up is a mistake many beginners make. Before any session ensure that you warm up thoroughly and mobilise the areas that you will be using during the workout. Furthermore, failure to warm up properly may increase the risk of injury.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>7. WILL POWER</b></h3> <p>Don’t give up. All beginners have set backs on the way to achieving their goals. Accept them and use them to re-motivate yourself.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>8. WATER</b></h3> <p>Ensure you drink plenty of water, before, during and after exercise, particularly if you are exercising in warm conditions or for long periods. Dehydration will result in a drop in performance and severe dehydration can be dangerous too.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>9. TRY SOMETHING NEW</b></h3> <p>Keep things interesting by trying new exercises, workouts and activities to keep your programme fresh, keep challenging your body and preventing boredom.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>10. TIME OF DAY</b></h3> <p>We are all different. Some of us rise early and some of us go to bed late. Exercise at the time when you feel that you have most energy</p>", "<br>";
+			 echo "<br>";
+
+			?>
+		</div>
+	</div>
+</body>
