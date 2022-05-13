56154ca725b79032d7136b306fb39039722d7260
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-14 07:51:50 +0800

    file abs has been added

---
+++ abs.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="program.php">Go Back</a></li>
+				<li><a href="chest.php">CHEST</a></li>
+				<li><a href="back.php">BACK</a></li>
+				<li><a href="leg.php">LEG</a></li>	
+				<li><a href="abs.php">ABS</a></li>	
+				<li><a href="biceps.php">BICEPS</a></li>	
+				<li><a href="triceps.php">TRICEPS</a></li>		
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1> ABDOMINAL <span> Workout </span></h1>
+		</div>
+		<div class="Ftips">
+			<?php
+
+			 echo "<h3><b>Benefits of Abdominal Workouts</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<p>It’s surprising how many people miss the abdominal muscles when working out. In doing so, they miss out on the numerous benefits of abdominal workouts. If you’ve been skipping your ab session, let’s take a look at some of the key benefits of ab workouts. </p>", "<br>";
+
+			 echo "<h3><b>Abdominal Strength Reduces Back Pain</b></h3> <p>Your abdominals act as an anchor for muscles of the mid and lower back. If your abs are weak, it forces the back muscles to work harder to support your middle, according to the Cleveland Clinic. Abdominal strength improves the endurance of the muscles of your back so you fatigue less easily and are less vulnerable to strain or injury.</p>"; "<br>";
+			 echo "<p>Strong abs also keep your belly from protruding too far forward and altering your center of gravity. If your abs are weak especially if you're overweight this forward lean stresses your back. </p>", "<br>";
+			 echo "<p>Strong abs help prevent an exaggerated anterior tilt to the pelvis. If you have this pelvic tilt, you may experience too much pressure on the lower back's discs and facet joints, flexible points that enable your back to bend. This pressure often leads to weakness, pain and potential rupture. Strong abs keep your vertebrae in alignment to prevent such deterioration. </p>", "<br>";
+
+			 echo "<h3><b>Supports Quality Posture</b></h3> <p>TWhen your abdominal muscles are weak, you may naturally tend to slouch because you don't have the musculature to support proper alignment. Strong abdominals support good posture by helping prop up your spine so it feels more natural to stand tall with your arms and legs in alignment.</p>"; "<br>";
+			 echo "<p>Proper posture lessens the wear and tear on your spine, which is a problem because it can lead to complications such as disc herniation. More benefits of better posture: You'll look taller, leaner and more confident, too. </p>", "<br>";
+			 echo "<p>According to the American Chiropractic Association, good posture also keeps the bones and joints in alignment, ensuring that muscles are properly engaged. Without this alignment, there may be abnormal wearing of the joint surfaces, potentially leading to degenerative arthritis and joint pain. Meanwhile, good posture reduces stress on the ligaments that hold the spinal joints in place, minimizing the risk of injury. </p>", "<br>";
+
+
+			 echo "<h3><b>Builds Functional Strength</b></h3>", "<br>";
+			 echo "<p>The benefits of abs go well beyond supporting your spine and low back. Simple actions, such as bending down to pick up a dropped object or tying your shoes, can be a chore if you have a weak middle.</p>", "<br>";
+			 echo "<p>Twisting, lifting and even getting up and down out of a chair requires the strength of your core. Building abdominal strength and endurance also makes household chores, such as mopping the floor and scrubbing the shower, easier, too, as you can bend, stand and crouch while moving your limbs with greater ease. </p>", "<br>";
+			 echo "<p>Injuries often occur during these mundane activities when your back has to bear more than its fair share of the work to complete such tasks. You may also experience injuries if you fall as a result of a weak core (more on that in section five).</p>", "<br>";
+
+			 echo "<h3><b>Improves Sports Performance</b></h3> <p>All of these moves emanate from strong abdominal muscles you have control when you perform moves intrinsic to the outcome of the game or race. You'll resist injury, but if you do get hurt, a strong core supports recovery and healing.</p>", "<br>";
+			 echo "<p>Abdominal strength and endurance also means you can potentially outperform and outlast your competition. Abdominal endurance is key to keeping pristine form for the duration of a marathon or through the second half of your soccer match.</p>", "<br>";
+			 echo "<p>Your abs are the center from which your joints activate. When your abs successfully stabilize your trunk for long periods of time, you minimize wasteful movement from your arms, legs and hips. Perseverant abs keep you efficient and in peak condition through grueling events.</p>", "<br>";
+
+
+			 echo "<h3><b>Creates Better Balance</b></h3> <p>The benefits of ab strength become particularly apparent anytime you find the need to balance and stabilize your body. Mayo Clinic notes that training your core allows the muscles in your pelvis, lower back, hips and abdomen to work harmoniously, leading to greater balance and stability.</p>", "<br>";
+			 echo "<p>Keep in mind, balance isn't just about standing on one leg and walking a tightrope. Balance comes into play every day whether you're walking on an uneven surface, riding on a bumpy road or catching yourself when taking a misstep off a curb.</p>", "<br>";
+			 echo "<p>Strong, resilient abdominals make such activities more pleasant, and potentially less disastrous. You can prevent falls or, if you do go down, be less likely to get seriously injured. For older adults who may have difficulty stabilizing themselves as they walk and perform everyday tasks, a strong core can help them reduce the risk of falls and serious injuries.</p>", "<br>";
+
+			 echo "<h3><b>BICEPS WORKOUT OF PERFECTION ROUTINE</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>1. HANGGING LEG RAISES </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 20</p>", "<br>";
+			 echo "<p>* Sets: 5</p>", "<br>";
+			 echo "<p>* Rest Time: 30 seconds</p>", "<br>";
+
+			 echo "<h3><b>2. MT. CLIMBERS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 50</p>", "<br>";
+			 echo "<p>* Sets: 2</p>", "<br>";
+			 echo "<p>* Rest Time: 30 seconds</p>", "<br>";
+
+			 echo "<h3><b>3. RUSSIAN TWIST</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 50</p>", "<br>";
+			 echo "<p>* Sets: 2</p>", "<br>";
+			 echo "<p>* Rest Time: 30 seconds</p>", "<br>";
+
+			 echo "<h3><b>4. HANGING FLUTTER KICKS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 20</p>", "<br>";
+			 echo "<p>* Sets: 5</p>", "<br>";
+			 echo "<p>* Rest Time: 30 seconds</p>", "<br>";
+
+			 echo "<h3><b>5. LEG RAISES </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 20</p>", "<br>";
+			 echo "<p>* Sets: 5</p>", "<br>";
+			 echo "<p>* Rest Time: 30 seconds</p>", "<br>";
+
+
+			?>
+		</div>
+	</div>
+</body>
