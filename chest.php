d40dead7549ffc0ef7b1e3e8e684e6995ae65ea0
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-13 21:35:56 +0800

    chest file has been added

---
+++ chest.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="program.php">Go Back</a></li>
+				<li><a href="chest.php">CHEST</a></li>
+				<li><a href="back.php">BACK</a></li>
+				<li><a href="leg.php">LEG</a></li>	
+				<li><a href=".php">ABS</a></li>	
+				<li><a href="biceps.php">BICEPS</a></li>	
+				<li><a href=".php">TRICEPS</a></li>	
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1> CHEST <span> Workout </span></h1>
+		</div>
+		<div class="Ftips">
+			<?php
+
+			 echo"<h3><b>1. HEAVY PROGRESSIVE OVERLOAD</b></h3> <p>Since your pectorals are a large muscle group, you need to train them with heavy resistance and progressive overload.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>What Does This Mean?</b></h3> <p>You have to lift heavy in the 3-8-rep range for the majority of your chest exercises. Despite what the guru’s and bodybuilding magazines say, this is how you build mass.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>Best Exercises</b></h3> <p>The best exercises include the dumbbell bench press, barbell bench press, dumbbell incline bench press, and the barbell incline bench press.If you don’t put a heavy load on your pecs, youwill not stimulate maximum hypertrophy and growth.</p>"; "<br>";
+			 echo "<p>It’s no secret that you absolutely have to be bench pressing and performing the chest related compound movements close to your 1 rep max.</p>"; "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>Rule of Thumb</b></h3> <p>You should make sure that 80 percent of your compound sets are done at 70-80+ percent of your 1 rep max. So, if your bench press max is 300 lbs you will want to make sure you are doing sets of 210-240 lbs at minimum. “Repping out” and chasing the “pump” in the 15-20 rep range may feel good, but the pump is short-lived, and it won’t translate into maximal muscle growth. Save it for the end of your workout and focus most of your efforts on training heavy. If you’ve spent years solely chasing the pump, you will experience a serious surge in gains by switching to this training methodology.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>2. HEAVY COMPOUND LIFTS</b></h3> <p>The biggest mistake that many people make with their chest workouts is that they don’t focus on big, compound lifts. How many of your chest days involve pec dec machines & 100’s of pec flyes? Here’s the truth: these exercises are the most effective at the end of your workout to add in some extra volume. They should never be the core focus of your chest day. You will never build thick muscle and strength with the pec dec machine. This is the problem with so many training routines. Stick with the proven basic lifts that work. If you want your chest to grow, stick with the big three: barbell and dumbbell bench press, barbell and dumbbell incline press, and heavy dips. No machines and no pec decs; at least not until you’ve put in the work on the heavy presses. Sorry guys, this is the key to full chest development.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>3. FOCUS ON FULL CHEST DEVELOPMENT</b></h3> <p>The most important rule to having a fully developed chest is that you must work the upper chest just as much as your lower chest. Since your upper chest muscles are the most stubborn for growth, they will need just as much attention. Not to mention, if your bench press has been plateaued for a while, hitting your upper chest hard may be exactly what you need to break through it. Most people focus 90-95% of their chest workout on the lower-mid pectoral muscle development without even knowing it. It should actually be closer to 50-50. The best chest workouts will grow the upper and lower sections equally.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>CHEST WORKOUT OF PERFECTION ROUTINE</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>1. BARBELL BENCH PRESS</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 5-8</p>", "<br>";
+			 echo "<p>* Rest Time: 90-180 seconds</p>", "<br>";
+
+			 echo "<h3><b>2. DUMBBELL INCLINE PRESS</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 8-12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>3. BARBELL INCLINE BENCH PRESS</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 6-10</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>4. WEIGHTED DIPS (CHEST VERSION)</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 8-12</p>", "<br>";
+			 echo "<p>* Rest Time: 2 minutes</p>", "<br>";
+
+			 echo "<h3><b>5. FLAT DUMBBELL PRESS</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 10-12</p>", "<br>";
+			 echo "<p>* Rest Time: 1 minute</p>", "<br>";
+
+
+			?>
+		</div>
+	</div>
+</body>
