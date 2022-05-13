21e923f1d306e00aa97cda442727337094f6d535
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-13 21:36:52 +0800

    leg file has been added

---
+++ leg.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="program.php">Go Back</a></li>
+				<li><a href="chest.php">CHEST</a></li>
+				<li><a href="back.php">BACK</a></li>
+				<li><a href="leg.php">LEG</a></li>	
+				<li><a href=".php">ABS</a></li>	
+				<li><a href="biceps.php">BICEPS</a></li>	
+				<li><a href=".php">TRICEPS</a></li>		
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1> LEG <span> Workout </span></h1>
+		</div>
+		<div class="Ftips">
+			<?php
+
+			 echo"<h3><b>Why You Shouldn’t Skip Your Leg Day</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<p>Nowadays, the issue of excess weight is extremely widespread. People all over the world aim to peel off unwanted pounds, look more attractive and improve their health status and self-esteem. There are thousands of weight-watchers in modern society, and all of them have their own fitness goals. Some people want to get a perfect waistline, while others want to reduce their inner thigh fat. Unfortunately, very often they neglect their legs, thinking that if they look nice, there is no need to work on this body part. However, this is one of the most common mistakes. In this article you will find all the benefits of leg workouts</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>5 reasons why leg day is so important</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<p>No one really looks forward to leg day. Of course, there are some exceptions bloody show offs but for the majority, it’s a necessary evil.</p>", "<br>";
+			 echo "<p>Get in, do your squats (but don’t rush, never rush your reps) and get out. Well, hobble out, slowly, grimacing in discomfort.</p>", "<br>";
+
+			 echo "<h3><b>1.You’ll build more muscle</b></h3> <p>No one wants to exhibit poor posture when they look at themselves in the mirror so the postural correction that comes from strong, activated back muscles is important, too.</p>"; "<br>";
+			 echo "<br>";
+			 echo "<p>Yes, cracking out the deadlifts will help build thicker slabs of meat.</p>", "<br>";
+			 echo "<p>Testosterone is a steroid hormone that is naturally produced in your body and plays a key role in the health of men, helping increase the growth of muscle mass and strength. Compound moves, such as squats and deadlifts, which use large muscle groups, help elevate your T-levels.</p>", "<br>";
+
+			 echo "<h3><b>2. You’ll burn more calories </b></h3> <p>Did you know that your gluteus maximus (aka, your backside) is the largest muscle in your body? Probably best to not tell your girlfriend this. They won’t take it as a compliment. But there is good news regarding the size of your derriere</p>", "<br>";
+			 echo "<br>";
+			 echo "<p>Working these bigger muscles requires more energy, which means your body will have to burn more calories.</p>", "<br>";
+
+			 echo "<h3><b>3. You won’t look like a chicken</b></h3> <p>Seriously, it’s not a good look.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>4. You’ll improve your big lifts</b></h3> <p>You might not believe it, but your legs and core produce the majority of your power during heaving lifting. Yes, even when you’re bench pressing, it is your lower body that is engaged to help provide a stable platform to pump from.</p>", "<br>";
+			 echo "<p>If you lack conditioning, your big lifts will only get so far. Performing functional exercises, like deadlifts, however, will strengthen your legs, glutes and lower body, while developing all-over muscle and mobility.</p>", "<br>";
+			 echo "<p>Remember, core strength supports the body in almost every movement. And the deadlift the King of Exercises is the key core strength building movement.</p>", "<br>";
+
+			 echo "<h3><b>5. You’ll reduce the risk of injury</b></h3> <p>Neglect your lower half and you are running the injury gauntlet.</p>", "<br>";
+			 echo "<p>Muscle imbalances, poorly conditioned hamstrings, and a lack of mobility will lead to a number of issues such as low back pain and ACL injuries. Squats, lunges and deadlifts will help develop your hamstrings, build muscle around weak joints and help promote stability and mobility.</p>", "<br>";
+			 echo "<p>Let’s be clear about this, chaps. If you are serious about building muscle, developing functional strength, and reducing the risk of injury, then you need to work on your legs. Often. If you're working out three times a week, one should be dedicated to your lower half.</p>", "<br>";
+
+			 echo "<h3><b>LEG WORKOUT OF PERFECTION ROUTINE</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>1. BACK SQUATS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 180 seconds</p>", "<br>";
+
+			 echo "<h3><b>2. LEG EXTENSION </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>3. LEG PRESS</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 180 seconds</p>", "<br>";
+
+			 echo "<h3><b>4. LEG CURLS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>5. CALF RAISE </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+
+			?>
+		</div>
+	</div>
+</body>
