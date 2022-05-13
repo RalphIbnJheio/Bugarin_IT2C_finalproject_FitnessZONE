95d144ee3fc24ada1c7985cea47d1ea85a0b4fa9
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-14 07:53:42 +0800

    triceps file has been added

---
+++ triceps.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="program.php">Go Back</a></li>
+				<li><a href="chest.php">CHEST</a></li>
+				<li><a href="back.php">BACK</a></li>
+				<li><a href="leg.php">LEG</a></li>	
+				<li><a href="abs.php">ABS</a></li>	
+				<li><a href="biceps.php">BICEPS</a></li>	
+				<li><a href="triceps.php">TRICEPS</a></li>		
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1> TRICEPS <span> Workout </span></h1>
+		</div>
+		<div class="Ftips">
+			<?php
+
+			 echo "<h3><b>What Do Strong Triceps Help With?</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<p>The triceps brachii, usually just called the triceps, is a three-headed muscle located at the back of the upper arm. These muscles primarily work to extend the elbow, and the long head helps the latissimus dorsi muscle, located on the back, to bring the arm toward the body. Strong triceps contribute to a powerful and toned upper body and can help you perform many daily functions and athletic activities with ease. </p>", "<br>";
+
+			 echo "<h3><b>Actions</b></h3> <p>Throwing or passing a ball and shooting a basketball are movements that require the use of the triceps. When you have strong triceps, you can do these actions with greater power. When the triceps contract isometrically to maintain a specific angle of the elbow, they help you do fine motor movement activities such as writing. Strong triceps allow you to maintain a stable position for longer.</p>"; "<br>";
+
+			 echo "<h3><b>Injury Prevention</b></h3> <p>The triceps are vulnerable to tendinitis, especially when you use them repeatedly in movements such as throwing or hammering. You should see your doctor if you experience nagging pain in the back part of the upper arm near the point of your elbow. Strengthening your triceps can help reduce the chance that you will develop this overuse injury because the muscles, rather than the joint, will take much of the responsibility for the action.</p>"; "<br>";
+
+			 echo "<h3><b>Upper Body Development</b></h3>", "<br>";
+			 echo "<br>";
+			 echo "<p>The triceps assist in the bench press and the pushup – two moves intrinsic to training the chest. If you want to improve on these exercises, you need to strengthen the triceps. Being able to hoist more in the bench press can lead to greater development of the chest. Being able to do more pushups may help you excel at fitness tests, such as those administered by the military. A stronger chest and triceps complex also allows you to perform pushing movements with ease. These can range from steering a stroller, wielding a shopping cart or tackling an opponent in football or rugby.</p>", "<br>";
+
+			 echo "<h3><b>Arm Girth </b></h3> <p>Your triceps make up approximately two-thirds of your entire upper arm – neglect these muscles and you are cheating yourself of serious strength. Some strength trainers, particularly bodybuilders, seek more girth in the upper arm. Strong triceps combined with strong biceps create this desired size.</p>", "<br>";
+
+			 echo "<h3><b>Strategies To Target Your Triceps Long Head And Build Bigger Arms!</b></h3> <p>There are two approaches you can take in your effort to build bigger triceps. First, there's the 'I need to do more and train harder' school of thought, in which more intensity and volume always seem to be the answer for your lagging body parts. This tactic works to a certain extent, but doing more has a point of diminishing—and then negative—returns.</p>", "<br>";
+			 echo "<p>The other school of thought is a bit more cerebral. It requires that you take apart the problem (the small triceps) and attack it from different perspectives—building up each of the three heads individually, as well as together—for maximum size and continued growth.</p>", "<br>";
+			 echo "<p>Since you're probably well-versed in the former approach, let's take a look at the latter. We'll start by dissecting the triceps muscle into its three heads: lateral, medial, and long. While it's less important to know where each is located on your arm, it's critical to know how best to target and emphasize specific heads.</p>", "<br>";
+			 echo "<p>We're going to focus primarily on the triceps long head, which attaches above the shoulder joint, unlike the other two triceps heads. This is important because the long head is stretched only when your arm is raised overhead. For a muscle to undergo a strong contraction, it must be fully stretched first. So when your arms are overhead, that's when you're really targeting the long head.</p>", "<br>";
+			 echo "<br>";
+			 echo "<p>1. Train Your Triceps Twice Per Week</p>", "<br>";
+			 echo "<p>2. Don't Always Tack On Triceps After Delts Or Chest</p>", "<br>";
+			 echo "<p>3. Choose A Solid Mass-Building Long-Head Move</p>", "<br>";
+			 echo "<p>4. Prioritize The Long Head</p>", "<br>";
+			 echo "<p>5. Choose A Second Long-Head Movement</p>", "<br>";
+
+			 echo "<h3><b>BICEPS WORKOUT OF PERFECTION ROUTINE</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>1. BICEP CURLS SQUATS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12-15</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>2. PREACHER CURLS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>3. HAMMER CURLS</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12-15</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>4. CONCENTRATION CURLS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>5. SEATED INCLINE CURLS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+
+			?>
+		</div>
+	</div>
+</body>
