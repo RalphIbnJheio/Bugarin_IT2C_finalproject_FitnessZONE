3cb786c46dec79010e83a55d5065892264062309
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-13 15:56:19 +0800

    HIIT file has been added

---
+++ HIIT.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="fitness.php">BACK</a></li>
+				<li><a href="tips.php">Beginner Fitness Tips</a></li>
+				<li><a href="workoutclothes.php">Workout Clothes</a></li>
+				<li><a href="HIIT.php">HIIT</a></li>	
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1> Benefits of <span> HIIT </span></h1>
+		</div>
+		<center><div class="Ftips">
+			<center><img src="photos/1clothes.JPG" class="1clothes"></center>
+			<?php
+
+			 echo"<h3><b>What is high intensity interval training?</b></h3> <p>HIIT involves short bursts of intense exercise alternated with low intensity recovery periods. Interestingly, it is perhaps the most time-efficient way to exercise.</p>", "<br>";
+			 echo "<br>";
+			 echo"<p>Typically, a HIIT workout will be 10–30 minutes in duration.</p>", "<br>";
+			 echo"<p>Despite how short the workout is, it can produce health benefits similar to twice as much moderate-intensity exercise.</p>", "<br>";
+			 echo"<p>The actual activity being performed varies but can include sprinting, biking, jumping rope, or other bodyweight exercises.</p>", "<br>";
+			 echo"<p>For example, a HIIT workout using a stationary bike could consist of 30 seconds of cycling as fast as possible with high resistance, followed by several minutes of slow, easy cycling with low resistance.</p>", "<br>";
+			 echo"<p>This would be considered one “round” or “repetition” of HIIT, and you would typically complete 4–6 reps in one workout.</p>", "<br>";
+			 echo"<p>The specific amount of time you exercise and recover will vary based on the activity you choose and how intensely you are exercising.</p>", "<br>";
+			 echo"<p>Regardless of how you implement this strategy, high intensity intervals should involve short periods of vigorous exercise that make your heart rate speed up.</p>", "<br>";
+			 echo"<p>HIIT not only provides the benefits of longer-duration exercise in a much shorter amount of time but also may provide some unique health benefits.</p>", "<br>";
+			 echo"<p>Typically, a HIIT workout will be 10–30 minutes in duration.</p>", "<br>";
+
+
+
+			 echo "<h3><b>THE BENEFITS OF HIIT WORKOUTS </b></h3> <p>Let’s start with the calories you’ll burn, which are many, not only during the workout but also in the hours afterwards. The latter comes from the excess post-exercise oxygen consumption (EPOC) effect, where your body burns more calories as it returns to its normal resting state after a workout and adapts to the exercise you’ve done. The EPOC effect increases with the intensity of the exercise you do, which is why HIIT is such an effective fat burner.</p>", "<br>";
+			 echo"<p>HIIT also increase your VO2 max, which is the amount of oxygen your body can use and is an indicator of cardio fitness. This is why any running or cycling training plan worth its salt has some form of interval training in it. Increasing your VO2 max is key to working harder for longer, helping you log a 5K personal best, for example.</p>", "<br>";
+			 echo"<p>There are also logistical benefits to HIIT, like the fact your workout takes less time so you can fit it into a lunch break. And while it’s tremendously hard work, the short, sharp challenge of HIIT ensures you’ll never get bored with your training.</p>", "<br>";
+			 echo "<br>";
+
+			?>
+		</div></center>
+	</div>
+</body>
