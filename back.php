1dbc46e8468c3f5ecb24671b41c667bbaa745d23
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-13 21:35:05 +0800

    back file has been added

---
+++ back.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="program.php">Go Back</a></li>
+				<li><a href="chest.php">CHEST</a></li>
+				<li><a href="back.php">BACK</a></li>
+				<li><a href="leg.php">LEG</a></li>	
+				<li><a href=".php">ABS</a></li>	
+				<li><a href="biceps.php">BICEPS</a></li>	
+				<li><a href=".php">TRICEPS</a></li>		
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1> BACK <span> Workout </span></h1>
+		</div>
+		<div class="Ftips">
+			<?php
+
+			 echo"<h3><b>THE BENEFITS OF TRAINING YOUR BACK MUSCLES</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>A More Balanced Body</b></h3> <p>A big chest looks ridiculous (and doesn't physically function all that well) without a big upper back and shoulders. Likewise, your abs are part of your core, whose role is ultimately to support your spine. For better function and aesthetics, your mid and lower back need attention.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>Stronger Back, Better Posture</b></h3> <p>No one wants to exhibit poor posture when they look at themselves in the mirror—so the postural correction that comes from strong, activated back muscles is important, too.</p>"; "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>A Strong Back Helps You to Be Stronger Everywhere</b></h3> <p>Your back muscles include your traps, lats, delts, rhomboids, erector spinae, and other muscles associated with the posterior chain. Targeting those muscles specifically is important for plenty of reasons—but your back muscles can also play a bigger role than you might expect in assisting big-time compound movements that pay off in spades, like the bench press. Your upper and mid-back muscles help to stabilize your shoulder joints, and the stronger and more stable your shoulders, the more weight you can lift in just about every upper-body exercise. Your hands have to be involved in back-focused exercises to handle the load, too, so don't be surprised when all those rows result in bigger arm muscles.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>The V-Taper</b></h3> <p>Are you fixated on your front because you're yearning for a V-shaped torsos? You're not going to get there without homing in on the back. If that's all you're looking to do, check out this workout but you should probably be motivated to look beyond just attaining that V.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<p>Now are you ready to turn your attention to your rear more? Get started with some of our favorite exercises to work your entire back.</p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>BACK WORKOUT OF PERFECTION ROUTINE</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>1. DEADLIFT </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 8-10</p>", "<br>";
+			 echo "<p>* Rest Time: 90-180 seconds</p>", "<br>";
+
+			 echo "<h3><b>2. LAT PULLDOWN</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>3. CABELE ROW</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>4. HYPER EXTENSION </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 8-12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>5. STIFF ARM PULLDOWN </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 1 minute</p>", "<br>";
+
+
+			?>
+		</div>
+	</div>
+</body>
