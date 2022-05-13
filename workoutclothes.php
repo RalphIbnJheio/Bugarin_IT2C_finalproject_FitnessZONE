2e8a03ce144d7f66af0ac1d5a0c7d189f1cb5875
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-13 15:36:00 +0800

    File workoutclothes has been added

---
+++ workoutclothes.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="fitness.php">BACK</a></li>
+				<li><a href="tips.php">Beginner Fitness Tips</a></li>
+				<li><a href="workoutclothes.php">Workout Clothes</a></li>
+				<li><a href="#Events">HIIT</a></li>		
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1>Workout <span> Clothes </span></h1>
+		</div>		
+		<div class="contentClothes">
+
+
+			<center><img src="photos/1clothes.JPG" class="1clothes"></center>
+			<p><b>SWEAT-WICKING & BREATHABILITY</b></p>
+			
+			<?php 
+				echo "<p>Due to the fact that you’re going to be sweating a lot during your workouts, it helps to have the appropriate clothes. And having breathable fabrics (like the specialty fabric found in Endeavor Athletic's apparel) that wick away moisture and keep you feeling cool and dry is going to make a tremendous difference in how you feel.</p>", "<br>";
+
+				echo "<p>Besides its sweat-wicking abilities, the performance fabric found in much of the Endeavor Athletic line is also great because it won’t promote bacteria growth. Less sweat and bacteria growth means no more smelly workout clothes!</p>","<br>";
+
+			?>
+
+			<center><img src="photos/2clothes.JPG" class="2clothes"></center>
+			<p><b>DURABILITY OF CLOTHES</b></p>
+
+			<?php
+				echo "<p>You don’t have to spend a ton of money to get great, long-lasting workout clothes. However, if you’re someone who loves to splash out on expensive leggings, this one may make you feel better. The right workout gear is typically going to be extra durable, allowing you to get plenty of use out of it. Compared to what you'd find at a typical department store or on a sale rack, performance clothing such as Endeavor Athletic's will last for a much longer time.</p>","<br>";
+
+			?>
+
+			<center><img src="photos/3clothes.JPG" class="3clothes"></center>
+			<p><b>PROTECTION FROM THE ENVIRONMENT</b></p>
+
+			<?php 
+				echo "<p>Your workout clothes can also help protect you from the environment. If you’re planning to exercise outside in the summer when it’s hot, loose clothing and breathable fabrics are incredibly important. It’s going to help keep your body cool so you don’t get overheated. Plus, you’ll want to opt for lighter colors to reflect the sun’s rays away from your body.</p>", "<br>";
+
+				echo "<p>Your workout clothes can also help protect you from the environment. If you’re planning to exercise outside in the summer when it’s hot, loose clothing and breathable fabrics are incredibly important. It’s going to help keep your body cool so you don’t get overheated. Plus, you’ll want to opt for lighter colors to reflect the sun’s rays away from your body.</p>", "<br>";
+
+			?>
+
+			<center><img src="photos/4clothes.JPG" class="4clothes"></center>
+			<p><b>COMFORT</b></p>
+
+			<?php 
+				echo "<p>One of the main benefits of wearing the right workout gear is comfort. The worst thing you can do is wear clothing or shoes that you aren’t comfortable in when you hit the gym. The wrong shoes will give your feet blisters. A sports bra that’s too tight will be digging into your back. And the wrong shorts are only going to rub against and irritate your thighs. Not fun!</p>", "<br>";
+
+				echo "<p>When you choose articles of clothing that provide you comfort in terms of the fabric and style, it makes a world of difference. You’ll feel confident in what you’re wearing, which allows you to direct your full attention to your workout instead of feeling self-conscious. Plus, it won’t cause you any discomfort that negatively impacts your performance.</p>", "<br>";
+
+			?>
+
+			
+			<center><img src="photos/5clothes.JPG" class="5clothes"></center>
+			<p><b>IMPROVED RANGE OF MOTION</b></p>
+
+			<?php 
+				echo "<p>Let’s face it, you aren’t going to go for a run in a pair of skinny jeans. They sure aren’t breathable and they definitely won’t be comfortable. But they also won’t provide you with the proper range of motion you need to complete your workout. The point being is that’s exactly what the wrong workout clothes are going to do to you because they’ll wind up holding you back.</p>","<br>";
+
+				echo "<p>For better performance, your best bet is to choose flexible clothing that allows your body to remove freely. Don’t go for anything too tight or fitted if you feel it’s holding you back. You want your workout clothes to form to you like a glove.</p>";
+
+			?>
+		</div>
+		
+	</div>
+</body>
