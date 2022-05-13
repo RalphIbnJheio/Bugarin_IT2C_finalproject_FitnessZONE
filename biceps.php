655db17f3335ba7a3f76245f92e138bdba14fbab
BugarinRalph <dyj12.oryxa@gmail.com>
2022-05-13 21:37:44 +0800

    biceps file has been addde

---
+++ biceps.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Fitness ZONE</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div class="wrapper-2">
+		<nav>
+			<img src="photos/logo.PNG" class="logo">
+			<ul>
+				<li><a href="program.php">Go Back</a></li>
+				<li><a href="chest.php">CHEST</a></li>
+				<li><a href="back.php">BACK</a></li>
+				<li><a href="leg.php">LEG</a></li>	
+				<li><a href=".php">ABS</a></li>	
+				<li><a href="biceps.php">BICEPS</a></li>	
+				<li><a href=".php">TRICEPS</a></li>		
+			</ul>
+		</nav>
+		<div class="main1">
+			<h1> BICEPS <span> Workout </span></h1>
+		</div>
+		<div class="Ftips">
+			<?php
+
+			 echo "<p>Big biceps signal to the world that you’ve put in some serious sweat equity at the gym. The biceps are small muscles, but to grow them takes time, knowledge, and intelligent programming. Also, your biceps flex your elbow which, when you think about it, is a pretty important job. Otherwise, how would you curl that beer (ahem, protein shake) to your lips or pick your kid up? The biceps don’t just look good; they serve a real function. </p>", "<br>";
+			 echo "<br>";
+			 echo "<p>The good news for you is training the biceps is pretty simple you curl, curl, and curl some more. That said, certain exercises get the job done better than others. To help you figure out which is best for you, we’ve culled 16 of the best biceps exercises onto one list, along with some more info on the muscle itself and how to incorporate biceps training into your routine. </p>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>How to Train Your Biceps</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<p>As for how often to train your biceps, you want to aim to get in about 10 to 14 total sets per week. If you’re a gym newbie, start with eight sets per week. Again, the biceps are small muscles with only two primary functions, so they don’t require a hefty amount of work to spark growth.</p>", "<br>";
+			 echo "<p>We suggest training your biceps with your triceps together as they’re antagonistic muscles (your biceps flex the elbow and your triceps extend the elbow). You can also save your biceps training for after your back workout or, if you follow a push, pull, legs split after your pulling day. Since your biceps are recruited in most upper body pulling movements, it makes sense to tack them onto that day since they’ll already be a bit fatigued.</p>", "<br>";
+
+			 echo "<h3><b>You’ll build more muscle</b></h3> <p>No one wants to exhibit poor posture when they look at themselves in the mirror so the postural correction that comes from strong, activated back muscles is important, too.</p>"; "<br>";
+			 echo "<br>";
+			 echo "<p>Yes, cracking out the deadlifts will help build thicker slabs of meat.</p>", "<br>";
+			 echo "<p>Testosterone is a steroid hormone that is naturally produced in your body and plays a key role in the health of men, helping increase the growth of muscle mass and strength. Compound moves, such as squats and deadlifts, which use large muscle groups, help elevate your T-levels.</p>", "<br>";
+
+			 echo "<h3><b>Biceps Training Rules</b></h3>", "<br>";
+			 echo "<br>";
+			 echo "<p>Your biceps play an important role in everyday life and play a pivotal role in upper body training  most notably back training. When training your biceps to grow or get stronger, there are a handful of rules that can help you improve your performance while limiting further risk of injury.</p>", "<br>";
+
+			 echo "<h3><b>Rule 1 — Stabilize </b></h3> <p>Establishing tension in the upper back (between the shoulder blades) during the setup of an exercise can help add stability to your upper body during biceps exercises. Increased stability external (outside the body) or internal (inside the body) can help you produce more significant tension during an exercise, leading to increased performance and more quality training volume.</p>", "<br>";
+
+			 echo "<h3><b>Rule 2 — Train Muscle Through a Full Range of Motion</b></h3> <p>To reap the full benefits of training the biceps (or any muscle), you need to ensure you fully contract the muscle by lifting through a complete range of motion. Different exercises present slightly different angles and ranges of motion but a general rule of thumb is to fully extend your arm and then curl the weight to your deltoid. The equipment you use matters, too. </p>", "<br>";
+			 echo "<br>";
+			 echo "<p>Free weights (barbells and dumbbells) usually tax the biceps in their mid-range position as these tools are heaviest when they’re farthest from the body. </p>", "<br>";
+
+			 echo "<h3><b>Rule 3 — Keep Your Elbows Fixed</b></h3> <p>A common mistake in biceps training is allowing the elbow to create unnecessary momentum and shoulder movement by not remaining fixed throughout the exercise. It can take away from the main functions of the biceps brachii (supination and elbow flexion), your overall upper body stability, and lead to a higher risk of potential injury.</p>", "<br>";
+
+			 echo "<h3><b>Rule 4 — Use Momentum (when appropriate)</b></h3> <p>If your goal is building muscle and strength and even endurance creating and maintaining significant amounts of tension in the target muscle(s) is imperative for stimulating positive muscular and neural adaptations. When you generate momentum, you set yourself up for bypassing the part of the rep that places the most significant amount of tension on the muscle working against the ultimate goal.</p>", "<br>";
+			 echo "<p>So, is it ever appropriate to create momentum within an exercise? It absolutely is. At the tail end of a hard set, momentum can be used to squeeze out another rep or two. You can use momentum during high load strength based training and low-moderate load power training to help drive neuromuscular adaptations that help build strength and power.</p>", "<br>";
+			 echo "<p>If your goal is to grow your biceps, start the exercise slowly and then accelerate through the rest of the rep — squeezing through the tension and creating a solid contraction in the biceps.</p>", "<br>";
+
+			 echo "<h3><b>BICEPS WORKOUT OF PERFECTION ROUTINE</b></h3>", "<br>";
+			 echo "<br>";
+
+			 echo "<h3><b>1. BICEP CURLS SQUATS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12-15</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>2. PREACHER CURLS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>3. HAMMER CURLS</b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12-15</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>4. CONCENTRATION CURLS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+			 echo "<h3><b>5. SEATED INCLINE CURLS </b></h3> <p>* Sets: 4</p>", "<br>";
+			 echo "<p>* Reps: 12</p>", "<br>";
+			 echo "<p>* Rest Time: 90 seconds</p>", "<br>";
+
+
+			?>
+		</div>
+	</div>
+</body>
